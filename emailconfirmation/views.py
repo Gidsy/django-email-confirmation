1e70f0b86c5358f482066c30dee6c430f112675f
Philipp Wassibauer <phil@maptales.com>
2011-06-21 22:05:54 +0200

    email confirmation has a redirect hook now, so instead of showing a validated screen a different screen and a django message can be displayed instead:

--- emailconfirmation/views.py
+++ emailconfirmation/views.py
@@ -6,6 +6,11 @@ from emailconfirmation.models import EmailConfirmation
 def confirm_email(request, confirmation_key):
     confirmation_key = confirmation_key.lower()
     email_address = EmailConfirmation.objects.confirm_email(confirmation_key)
+    
+    next = getattr(settings, "EMAIL_CONFIRMATION_REDIRECT_URL", False)
+    if next:
+        return HttpResponseRedirect(next)
+        
     return render_to_response("emailconfirmation/confirm_email.html", {
         "email_address": email_address,
     }, context_instance=RequestContext(request))
