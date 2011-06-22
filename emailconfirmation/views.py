55f65a660ffc9f6d0a51a5235c3136e0b4859fa7
Philipp Wassibauer <phil@maptales.com>
2011-06-22 17:42:27 +0200

    notify user if he confirmed email attached to different account. add the setting to redirect to profile attached to email

--- emailconfirmation/views.py
+++ emailconfirmation/views.py
@@ -1,13 +1,24 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-
+from django.http import HttpResponseRedirect
 from emailconfirmation.models import EmailConfirmation
 from django.conf import settings
+from django.contrib import messages
+
 
 def confirm_email(request, confirmation_key):
     confirmation_key = confirmation_key.lower()
     email_address = EmailConfirmation.objects.confirm_email(confirmation_key)
     
+    # if user is logged in check if the confirmed email is attached to his account
+    # notify him if not
+    if request.user.is_authenticated():
+        if request.user != email_address.user:
+            messages.warning(request, _("The email you confirmed does not belong to the account you are signed in with."))
+                
+    if getattr(settings, "EMAIL_CONFIRMATION_REDIRECT_TO_PROFILE", False):
+        return HttpResponseRedirect(email_address.user.get_absolute_url)
+                
     next = getattr(settings, "EMAIL_CONFIRMATION_REDIRECT_URL", False)
     if next:
         return HttpResponseRedirect(next)
