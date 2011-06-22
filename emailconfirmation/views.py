00799d0007adc2313b7b6201ff1f72bab1c65715
Philipp Wassibauer <phil@maptales.com>
2011-06-22 17:16:33 +0200

    settings import missing

--- emailconfirmation/views.py
+++ emailconfirmation/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 
 from emailconfirmation.models import EmailConfirmation
+from django.conf import settings
 
 def confirm_email(request, confirmation_key):
     confirmation_key = confirmation_key.lower()
