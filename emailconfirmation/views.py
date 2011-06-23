58ab7bd9aa69073cf4f80d66b9eaddcc6cdb4382
Philipp Wassibauer <phil@gidsy.com>
2011-06-23 16:27:28 +0200

    success message added if user is redirected to profile after confirmation, version bumped

--- emailconfirmation/views.py
+++ emailconfirmation/views.py
@@ -4,23 +4,27 @@ from django.http import HttpResponseRedirect
 from emailconfirmation.models import EmailConfirmation
 from django.conf import settings
 from django.contrib import messages
-
+from django.utils.translation import ugettext as _
 
 def confirm_email(request, confirmation_key):
     confirmation_key = confirmation_key.lower()
     email_address = EmailConfirmation.objects.confirm_email(confirmation_key)
-    
+            
     # if user is logged in check if the confirmed email is attached to his account
     # notify him if not
     if request.user.is_authenticated():
         if request.user != email_address.user:
-            messages.warning(request, _("The email you confirmed does not belong to the account you are signed in with."))
+            messages.warning(request, _("The email you confirmed does not belong \
+                                        to the account you are signed in with."))
                 
     if getattr(settings, "EMAIL_CONFIRMATION_REDIRECT_TO_PROFILE", False):
         return HttpResponseRedirect(email_address.user.get_absolute_url())
                 
     next = getattr(settings, "EMAIL_CONFIRMATION_REDIRECT_URL", False)
     if next:
+        messages.warning(request, \
+                     _("You successfully confirmed your email address %(email)s")\
+                                                 %{'email':email_address.email})
         return HttpResponseRedirect(next)
         
     return render_to_response("emailconfirmation/confirm_email.html", {
