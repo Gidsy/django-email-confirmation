8c063ada703b0aafdf98f44472e8a0290bfc7fd9
Philipp Wassibauer <phil@gidsy.com>
2011-07-07 15:05:38 +0200

    message for redirects added

--- emailconfirmation/views.py
+++ emailconfirmation/views.py
@@ -30,16 +30,19 @@ def confirm_email(request, confirmation_key):
             if request.user != email_address.user:
                 messages.warning(request, _("The email you confirmed does not belong \
                                             to the account you are signed in with."))
-                    
-        if getattr(settings, "EMAIL_CONFIRMATION_REDIRECT_TO_PROFILE", False):
-            return HttpResponseRedirect(email_address.user.get_absolute_url())
-                    
+        
+        redirect_to_profile = getattr(settings, "EMAIL_CONFIRMATION_REDIRECT_TO_PROFILE", False)
         next = getattr(settings, "EMAIL_CONFIRMATION_REDIRECT_URL", False)
-        if next:
-            messages.warning(request, \
+        
+        if next or redirect_to_profile:
+            messages.success(request, \
                          _("You successfully confirmed your email address %(email)s")\
                                                      %{'email':email_address.email})
-            return HttpResponseRedirect(next)
+            if redirect_to_profile:
+                return HttpResponseRedirect(email_address.user.get_absolute_url())
+                        
+            if next:
+                return HttpResponseRedirect(next)
         
     return render_to_response("emailconfirmation/confirm_email.html", {
         "email_address": email_address,
