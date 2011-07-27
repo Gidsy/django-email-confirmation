9b6caf877a7652b2f7f2a39ae32e93c2a3ce28b2
Philipp Wassibauer <phil@gidsy.com>
2011-07-27 16:54:35 +0200

    modified templatetag a bit

--- emailconfirmation/templatetags/emailconfirmation_tags.py
+++ emailconfirmation/templatetags/emailconfirmation_tags.py
@@ -1,10 +1,13 @@
 from django.template import Context, Template
 from django.template.loader import get_template
 from django import template
-from models import EmailAddress
+from emailconfirmation.models import EmailAddress
 
 register = template.Library()
 
-@register.simple_tag()
+@register.filter
 def has_verified_email(user):
-    return EmailAddress.objects.get_primary(user)
+    primary_email = EmailAddress.objects.get_primary(user)
+    if primary_email:
+        return primary_email.verified
+    return False
