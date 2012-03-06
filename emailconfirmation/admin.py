82363b7a3d10cbf5834922b74ad386faf7e52bf8
Carlo Smouter <carlo@gidsy.com>
2012-03-06 17:50:30 +0100

    Added search functionality for usernames and email addresses. Updated version number

--- emailconfirmation/admin.py
+++ emailconfirmation/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from emailconfirmation.models import EmailAddress, EmailConfirmation
 
-admin.site.register(EmailAddress)
+
+class EmailAddressAdmin(admin.ModelAdmin): 
+    list_display = ['email', 'user']
+    search_fields = ['email', 'user__username']
+
+admin.site.register(EmailAddress, EmailAddressAdmin)
 admin.site.register(EmailConfirmation)
